c4c95c2d1fe65bad250f057ac57c89822f841afc
David Gomez-Urquiza <david.gurquiza@gmail.com>
2012-11-04 15:55:40 -0600

    Tabs to spaces

--- django_env_functions.sh
+++ django_env_functions.sh
@@ -2,47 +2,45 @@
 #
 # Helper functions for Django projects in virtual env
 #
-# include in your .bashrc file:
-#
 # . ~/django_env_functions.sh
 # https://docs.djangoproject.com/en/1.4/ref/django-admin/
 # https://gist.github.com/2889330
 
 function myenv() {
-	deactivate
-	source venv/bin/activate
+    deactivate
+    source venv/bin/activate
     export DJANGO_SETTINGS_MODULE="settings"
 }
 
 function testenv() {
-	deactivate
-	source venv/bin/activate
+    deactivate
+    source venv/bin/activate
     export DJANGO_SETTINGS_MODULE="test_settings"
 }
 
 function freeze(){
-	pip freeze > requirements.txt
+    pip freeze > requirements.txt
 }
 
 function dj() {
-	# run arbitrary django commands
-	# python django-admin.py $*
-	python manage.py $*
+    # run arbitrary django commands
+    # python django-admin.py $*
+    python manage.py $*
 }
 
 function djsh() {
-	# run shell for active environment
-	#django-admin.py shell --settings $DJANGO_SETTINGS_MODULE
-	dj shell
+    # run shell for active environment
+    #django-admin.py shell --settings $DJANGO_SETTINGS_MODULE
+    dj shell
 }
 
 function djdb() {
-	# run db shell for active environment
-	dj dbshell
+    # run db shell for active environment
+    dj dbshell
 }
 
 function collectstatic() {
-	# run collect static for environment & automatically say yes
-	#django-admin.py collectstatic --noinput --settings $DJANGO_SETTINGS_MODULE
-	dj collectstatic --noinput
+    # run collect static for environment & automatically say yes
+    #django-admin.py collectstatic --noinput --settings $DJANGO_SETTINGS_MODULE
+    dj collectstatic --noinput
 }
